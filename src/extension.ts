0a8c13a7f1f4029f981a99ab26edbd552c80417e
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-04 18:32:36 -0300

    ✨  feat: add command save as template

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,13 @@
 import * as vscode from 'vscode';
+
 import myTemplates from './commands/myTemplates';
+import saveAsTemplate from './commands/saveAsTemplate';
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('simple-file.getTemplate', myTemplates);
-
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand('simple-file.getTemplate', myTemplates),
+    vscode.commands.registerCommand('simple-file.saveAsTemplate', saveAsTemplate)
+  );
 }
 
 export function deactivate() { }
